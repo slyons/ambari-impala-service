e6e1ccf1ca40999692c817014f1c745277bd8dbd
Scott Lyons <scottalyons@gmail.com>
2016-05-31 10:56:09 -0700

    Adding missing config files
    Adding missing config files from other HDFS services

--- package/scripts/impala-daemon.py
+++ package/scripts/impala-daemon.py
@@ -37,6 +37,37 @@ class ImpalaDaemon(Script):
         env_content = InlineTemplate(params.impala_defaults_content)
         File("/etc/default/impala", content=env_content, owner=params.impala_user, group=params.impala_group)
 
+        XmlConfig("hdfs-site.xml",
+            conf_dir=params.impala_conf_dir,
+            configurations=params.config['configurations']['hdfs-site'],
+            configuration_attributes=params.config['configuration_attributes']['hdfs-site'],
+            owner=params.impala_user,
+            group=params.impala_group
+        )
+
+        XmlConfig("core-site.xml",
+            conf_dir=params.impala_conf_dir,
+            configurations=params.config['configurations']['core-site'],
+            configuration_attributes=params.config['configuration_attributes']['core-site'],
+            owner=params.impala_user,
+            group=params.impala_group,
+            mode=0644
+        )
+
+        XmlConfig("hbase-site.xml",
+            conf_dir = params.impala_conf_dir,
+            configurations = params.config['configurations']['hbase-site'],
+            configuration_attributes=params.config['configuration_attributes']['hbase-site']
+        )
+
+        XmlConfig("hive-site.xml",
+          conf_dir=params.impala_conf_dir,
+          configurations=params.config['configuration_attributes']['hive-site'],
+          owner=params.impala_user,
+          group=params.impala_group,
+          mode=0644
+        )
+
 
     #Call start.sh to start the service
     def start(self, env):
@@ -56,6 +87,13 @@ class ImpalaDaemon(Script):
         cmd = 'service impala-server status'
         Execute('echo "Running cmd: ' + cmd + '"')
         Execute(cmd)
+
+    def setup_config(self, env):
+        import params
+        env.set_params(params)
+
+
+        
 
 if __name__ == "__main__":
     ImpalaDaemon().execute()
